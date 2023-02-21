e518ca571254c01da7161408db94bfc707d78c5f
Polizei321 <124945613+Polizei321@users.noreply.github.com>
2023-02-21 01:13:11 -0500

    Update nmcm.lua

--- nmcm.lua
+++ nmcm.lua
@@ -20,7 +20,7 @@ if not status then
 end
 if auto_updater == true then error("Invalid auto-updater lib. Please delete your Stand/Lua Scripts/lib/auto-updater.lua and try again") end
 auto_updater.run_auto_update({
-    source_url="https://raw.githubusercontent.com/Polizei321/network-tools/main/NetworkTools",
+    source_url="https://raw.githubusercontent.com/Polizei321/network-tools/main/nmcm.lua",
     script_relpath=SCRIPT_RELPATH,
     verify_file_begins_with="--"
 })
